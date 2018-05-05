0aedb4d7b8f99f229be89441c30d8527be649835
flybyme <mangoraft@gmail.com>
2018-05-05 07:26:50 -0500

    update sizes to es6/await

--- routes/other/sizes.js
+++ routes/other/sizes.js
@@ -14,28 +14,24 @@ var select = 'is_active created_at updated_at name logSession metricSession main
  */
 
 routes.push({
-	meta : {
-		method : 'GET',
-		paths : ['/sizes'],
-		version : '1.0.0',
-		auth : true,
-		//role : 'admin'
-	},
-	middleware : function(req, res, next) {
+    meta: {
+        method: 'GET',
+        paths: ['/sizes'],
+        version: '1.0.0',
+        auth: true
+    },
+    middleware: async function (req, res, next) {
+        let [err, sizes] = await req.to(req.req.mongoose.Size.find())
 
-		req.mongoose.Size.find({
+        if (err) {
+            return next(new restify.errors.InternalError(err.message || err));
+        }
+        res.json({
+            status: "success",
+            result: sizes.map(req.format.size)
+        });
 
-		}, function(err, sizes) {
-			if (err) {
-				return next(new restify.errors.InternalError(err.message||err));
-			}
-			res.json({
-				status : "success",
-				result : sizes.map(req.format.size)
-			});
-		});
-
-	}
+    }
 });
 /**
  * GET /organization/:organization/zone
@@ -43,32 +39,30 @@ routes.push({
  */
 
 routes.push({
-	meta : {
-		method : 'GET',
-		paths : ['/sizes/:name'],
-		version : '1.0.0',
-		auth : true,
-		//role : 'admin'
-	},
-	middleware : function(req, res, next) {
-		var name = req.params.name;
+    meta: {
+        method: 'GET',
+        paths: ['/sizes/:name'],
+        version: '1.0.0',
+        auth: true
+    },
+    middleware: async function (req, res, next) {
+        let {name} = req.params;
 
-		req.mongoose.Size.findOne({
-			name : name
-		}, function(err, size) {
-			if (err) {
-				return next(new restify.errors.InternalError(err.message||err));
-			}
-			if (!size) {
-				return next(new restify.errors.NotFoundError('Size ' + name + ' not found'));
-			}
-			res.json({
-				status : "success",
-				result : req.format.size(size)
-			});
-		});
+        let [err, size] = await req.to(req.req.mongoose.Size.findOne({
+            name: name
+        }))
 
-	}
+        if (err) {
+            return next(new restify.errors.InternalError(err.message || err));
+        }
+        if (!size) {
+            return next(new restify.errors.NotFoundError('Size ' + name + ' not found'));
+        }
+        res.json({
+            status: "success",
+            result: req.format.size(size)
+        });
+    }
 });
 
 /**
